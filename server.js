92296a0f4fcf320226738129ecbf704ace7a79ad
kushdilip <kush.dilip@gmail.com>
2013-11-07 01:53:05 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,49 @@
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express');
+var http = require('http');
+var passport = require('passport')
+var path = require('path');
+
+var routes = require('./routes');
+var user = require('./routes/user');
+
+var app = express();
+
+// all environments
+//app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, '/client/views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+//app.use(app.router);
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.cookieSession(
+    {
+        secret: process.env.COOKIE_SECRET || "Superdupersecret"
+    }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+// // development only
+// if ('development' == app.get('env')) {
+//   app.use(express.errorHandler());
+// }
+
+require('./server/routes.js')(app);
+
+// app.get('/', routes.index);
+// app.get('/users', user.list);
+
+app.set('port', process.env.PORT || 3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
